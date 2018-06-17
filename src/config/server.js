0303cd7538fe092ac9fabbef1462cf990b3934ce
Marcelo Souza <chariondm@gmail.com>
2018-06-17 20:33:21 -0300

    Enable CORs

--- src/config/server.js
+++ src/config/server.js
@@ -1,5 +1,6 @@
 const bodyParser = require('body-parser');
 const express = require('express');
+const allowCors = require('./cors');
 
 const port = 3000;
 
@@ -7,6 +8,7 @@ const server = express();
 
 server.use(bodyParser.urlencoded({ extended: true }));
 server.use(bodyParser.json());
+server.use(allowCors);
 
 server.listen(port, () => console.log(`BACKEND is running on port ${ port }`));
 
